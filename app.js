d0bddff1188627f3213686b5a16a3b83912fe015
sojazzzzzy <jazmynewashington@gmail.com>
2023-03-13 20:55:52 -0700

    added 5 basic coktails to appjs file

---
+++ app.js
@@ -0,0 +1,43 @@
+let drinks = [
+	{
+		name: 'Vodka Tonic',
+		ingredients: ['1 part Vodka', '3 parts Tonic water'],
+		garnish: 'Lime or lemon',
+		glassware: 'Highball glass',
+	},
+
+	{
+		name: 'Gin & Tonic',
+		ingredients: ['1 part Gin', '3 parts Tonic water'],
+		garnish: 'Slice of Lime',
+		glassware: 'Highball glass',
+	},
+
+	{
+		name: 'Whiskey Sour',
+		ingredients: [
+			'3 parts Bourbon whiskey',
+			'1 dash egg white',
+			'2 parts fresh lemon juice',
+			'1 part Gomme syrup',
+		],
+		garnish: 'Lemon rind, Maraschino cherry, orange slice, sugared glass',
+		glassware: 'Cobbler, Old fashioned glass',
+	},
+
+	{
+		name: 'Margarita',
+		ingredients: ['1 part lime juice', '1 part triple sec', '2 parts Tequila'],
+		garnish: 'Lime slice, salt on the rim',
+		glassware: 'Margarita glass',
+	},
+
+	{
+		name: 'Paloma',
+		ingredients: '1 part Tequila, 3 parts grapefruit soda',
+		garnish: 'Lime wedge',
+		glassware: 'Highball glass',
+	},
+];
+
+console.log(drinks);
